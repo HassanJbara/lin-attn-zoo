fix numerical issues in deltanet model

models/deltanet.py
@@ -46,8 +46,17 @@ class DeltaNet(nn.Module):
         self.v_conv1d = self._build_conv(self.proj_dim)
 
         self.activation = nn.SiLU()
-        self.o_norm = nn.RMSNorm(self.head_dim, eps=self.norm_eps)
+        self.o_norm = nn.RMSNorm(self.head_dim, eps=self.norm_eps, dtype=torch.float32)
         self.o_proj = nn.Linear(self.proj_dim, self.hidden_size, bias=False)
+
+        I = torch.eye(self.chunk_size)  # [C, C]
+        M = torch.tril(torch.ones((self.chunk_size, self.chunk_size)))  # [C, C]
+        self.register_buffer(
+            "_chunk_I", I[None, None, :, :], persistent=False
+        )  # [1,1,C,C]
+        self.register_buffer(
+            "_chunk_M", M[None, None, :, :], persistent=False
+        )  # [1,1,C,C]
 
     def _build_conv(self, conv_dim: int) -> nn.Conv1d:
         return nn.Conv1d(
@@ -82,24 +91,20 @@ class DeltaNet(nn.Module):
     ) -> torch.Tensor:
         """Process tensors through convolution and reshape for attention."""
         return self._calculate_conv(x, conv_layer).reshape(
-            B, L, self.num_heads, self.head_dim
+            B, L, self.num_heads, self.head_dim, 1
         )
 
     def _delta_rule(
         self,
-        k: torch.Tensor,  # Shape: [B, H, D]
-        q: torch.Tensor,  # Shape: [B, H, D]
-        v: torch.Tensor,  # Shape: [B, H, D]
-        beta: torch.Tensor,  # Shape: [B, H, 1]
-        S: torch.Tensor,  # Shape: [B, H, D, D]
+        k: torch.Tensor,  # Shape: [B, 1, H, D, 1]
+        q: torch.Tensor,  # Shape: [B, 1, H, D, 1]
+        v: torch.Tensor,  # Shape: [B, 1, H, D, 1]
+        S: torch.Tensor,  # Shape: [B, 1, H, D, D]
+        beta: torch.Tensor,  # Shape: [B, 1, H, 1, 1]
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        correction = torch.einsum("bhk,bhkv->bhv", k, S)
-        v = (v - correction) * beta
-
-        # Update hidden state with outer product using einsum
-        S = S + torch.einsum("bhk,bhv->bhkv", k, v)
-        o = torch.einsum("bhk,bhkv->bhv", q, S)
-
+        update = (S @ k - v) @ k.transpose(-1, -2)  # [B, 1, H, D, D]
+        S = S - beta * update
+        o = (S @ q / (self.head_dim**0.5)).squeeze(-1)  # [B, 1, H, D]
         return o, S
 
     def _get_chunk(self, x: torch.Tensor, idx: int) -> torch.Tensor:
@@ -112,14 +117,16 @@ class DeltaNet(nn.Module):
         q: torch.Tensor,
         k: torch.Tensor,
         v: torch.Tensor,
-        S: torch.Tensor,  # Shape: [B, 1, H, D, D]
+        S: torch.Tensor,
         beta: torch.Tensor,
         o: torch.Tensor,
     ) -> torch.Tensor:
-        (B, L, H, D) = q.shape
+        # dimensions same as in _delta_rule except for L at dim 1
+        (B, L, H, D, _) = q.shape
         n_chunks = ceil(L / self.chunk_size)
         last_size = L % self.chunk_size
-        q, k, v, beta = map(lambda x: x.transpose(1, 2), (q, k, v, beta))
+        q, k, v = map(lambda x: x.transpose(1, 2).reshape(B, H, L, D), (q, k, v))
+        beta = beta.transpose(1, 2).reshape(B, H, L, 1)
 
         padding_needed = self.chunk_size - last_size if last_size > 0 else 0
         if padding_needed > 0:
@@ -127,6 +134,7 @@ class DeltaNet(nn.Module):
                 lambda x: F.pad(x, (0, 0, 0, padding_needed)), (q, k, v, beta)
             )
 
+        q = q / (self.head_dim**0.5)
         S = S.squeeze(dim=1)  # [B, H, D, D]
         I = torch.eye(self.chunk_size).repeat(B, H, 1, 1).to(q.device).type(q.dtype)
         M = (
@@ -182,8 +190,7 @@ class DeltaNet(nn.Module):
         v = self._reshape_for_attention(v, self.v_conv1d, B, L)
 
         k, q = self._l2_normalize(k), self._l2_normalize(q)
-        q = q / (self.head_dim**0.5)
-        beta = self._calculate_beta(x).reshape(B, L, self.num_heads, 1)
+        beta = self._calculate_beta(x).reshape(B, L, self.num_heads, 1, 1)
 
         if last_state is None:
             last_state = x.new_zeros(
@@ -200,10 +207,14 @@ class DeltaNet(nn.Module):
         else:
             outputs = []
             for t in range(L):
-                beta_t, k_t, q_t, v_t = (beta[:, t], k[:, t], q[:, t], v[:, t])
-                o_t, last_state = self._delta_rule(
-                    k_t, q_t, v_t, beta_t, last_state.squeeze(dim=1)
+                t_slice = slice(t, t + 1)
+                beta_t, k_t, q_t, v_t = (
+                    beta[:, t_slice],
+                    k[:, t_slice],
+                    q[:, t_slice],
+                    v[:, t_slice],
                 )
+                o_t, last_state = self._delta_rule(k_t, q_t, v_t, last_state, beta_t)
                 outputs.append(o_t)
             o = torch.cat(outputs, dim=1)  # [B, L, H, D]
 